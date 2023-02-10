86df2c34e661fb71e2b58f58af10fa08aa37bca8
cvashanku <cvashankar18@gmail.com>
2023-02-10 22:15:39 +0530

    observer design pattern

---
+++ behavioral-dp/observer-dp/Observer.java
@@ -0,0 +1,7 @@
+//create observer class
+
+public abstract class Observer 
+{
+   protected Subject subject;
+   public abstract void update();
+}
---
+++ behavioral-dp/observer-dp/OctalObserver.java
@@ -0,0 +1,16 @@
+//create concrete observer classes
+
+public class OctalObserver extends Observer
+{
+
+   public OctalObserver(Subject subject)
+   {
+      this.subject=subject;
+      this.subject.attach(this);
+   }
+
+   public void update() 
+   {
+     System.out.println("Octal String: " + Integer.toOctalString(subject.getState())); 
+   }
+}
